ecec2a491ff77338928357d58d4dbb4bf9fe3f98
lucasasilva <las.lucasantonio@gmail.com>
2025-06-11 22:07:20 -0300

    segunda iteração do front, com gráfico

--- src/main/java/com/tickets/dash_tickets/controller/DTO/TicketsDTO.java
+++ src/main/java/com/tickets/dash_tickets/controller/DTO/TicketsDTO.java
@@ -14,16 +14,17 @@ import java.time.LocalDate;
 public class TicketsDTO {
     private Long id;
     private String nome;
-    private Integer codCliente;
-    private Integer codModulo;
+    //precisa levar o nome do cliente e do módulo no front. Mais fácil já trazer aqui junto aos tickets;
+    private Clientes codCliente;
+    private Modulo codModulo;
     private LocalDate dataAbertura;
     private LocalDate dataEncerramento;
 
     public TicketsDTO(int id, String nome, Clientes codCliente, Modulo codModulo, LocalDate dataAbertura, LocalDate dataEncerramento) {
         this.id = (long) id;
         this.nome = nome;
-        this.codCliente = codCliente != null ? codCliente.getId() : null;
-        this.codModulo = codModulo != null ? codModulo.getId() : null;
+        this.codCliente = codCliente;
+        this.codModulo = codModulo;
         this.dataAbertura = dataAbertura;
         this.dataEncerramento = dataEncerramento;
     }
--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -4,31 +4,73 @@ window.onload = () => {
 
 async function getData(params) {
   const url = "http://localhost:8080/retornar-tickets";
-  let response = 0;
-  response = await fetch(url, {
+  let response = await fetch(url, {
     method: "POST",
     headers: { "Content-type": "application/json" },
     body: JSON.stringify({ mes: 4, ano: 2021 }),
   });
   const dados = await response.json();
 
-  const elementoHTML = document.querySelector("#tickets");
+   //criar gráfico
+   const ctx = document.getElementById("grafico-clientes");
+   function criarGrafico(dadosJson) {
+     new Chart(ctx, {
+       type: "doughnut",
+       data: {
+         labels: dadosJson.map(row => row.ticketsPorCliente.forEach(item => {item.cliente})),
+         datasets: [
+           {
+             label: "# of Votes",
+             data:dadosJson[0].ticketsPorCliente.map(item=> item.totalTicketsCliente),
+             borderWidth: 1,
+           },
+         ],
+       },
+       options: {
+         scales: {
+           y: {
+             beginAtZero: true,
+           },
+         },
+       },
+     });
+   }
+   criarGrafico(dados)
+   console.log(dados[0].ticketsPorCliente[0].totalTicketsCliente)
+
+
+
+  const elementoHTML = document.getElementById("cabecalho-tickets");
 
   const listaTickets = dados[0].tickets;
   if (elementoHTML) {
-    let html = "<ul>";
-    listaTickets.forEach(ticket => {
-      html += `
-            <li>
-            id: ${ticket.id}  
-            Nome: ${ticket.nome}
-            </li>
-            `;
+    //let html =
+    listaTickets.forEach((ticket) => {
+      document.getElementById("id").innerHTML += `${ticket.id} <br>`;
+      document.getElementById("Titulo").innerHTML += `${ticket.nome}<br>`;
+      document.getElementById(
+        "Cliente"
+      ).innerHTML += `${ticket.codCliente.nome}<br>`;
+      document.getElementById("dtAbertura").innerHTML += `${ticket.dataAbertura
+        .split("-")
+        .reverse()
+        .join("/")}<br>`;
+      document.getElementById(
+        "dtEncerramento"
+      ).innerHTML += `${ticket.dataEncerramento
+        .split("-")
+        .reverse()
+        .join("/")}<br>`;
+      document.getElementById(
+        "Modulo"
+      ).innerHTML += `${ticket.codModulo.nome}<br>`;
     });
-    html += "</ul>";
+    //html += "</div>";
 
-    elementoHTML.innerHTML = html;
-  }else{
-    console.log("nenhum elemento chamado tickets encontrado")
+    //elementoHTML.innerHTML = html;
+
+   
+  } else {
+    console.log("nenhum elemento chamado tickets encontrado");
   }
 }
